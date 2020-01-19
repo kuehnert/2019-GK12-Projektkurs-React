033a4134327212512ee5058934a1b707e16e24e7
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-01-19 11:56:51 +0100

    render course student table

--- src/features/courses/CoursePage.tsx
+++ src/features/courses/CoursePage.tsx
@@ -1,14 +1,16 @@
-import { Fab, Theme, Typography } from '@material-ui/core';
+import { TableRow, Fab, Table, TableCell, TableHead, Theme, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { Edit as EditIcon } from '@material-ui/icons';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
+import { RootState } from '../../app/rootReducer';
 import Loading from '../../components/Loading';
-import { useCourse, useTerm } from "../../utils/selectors";
+import history from '../../history';
+import { useCourse, useTerm } from '../../utils/selectors';
 import { getTerm } from '../terms/termSlice';
 import { getCourses } from './courseSlice';
-import history from '../../history';
+import { getEnrolments } from './enrolmentSlice';
 
 interface MatchParams {
   termId: string;
@@ -20,13 +22,27 @@ export default (props: RouteComponentProps<MatchParams>) => {
   const dispatch = useDispatch();
   const term = useTerm(termId);
   const course = useCourse(termId, courseId);
+  const enrolments = useSelector((state: RootState) => state.enrolments.enrolments[courseId]);
   const classes = useStyles();
-  useEffect(() => {
-    dispatch(getTerm(termId));
-    dispatch(getCourses(termId));
-  }, [dispatch, termId, courseId]);
 
-  if (term == null || course == null) {
+  useEffect(() => {
+    if (term == null) {
+      dispatch(getTerm(termId));
+    }
+
+    if (course == null) {
+      dispatch(getCourses(termId));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (course != null) {
+      dispatch(getEnrolments(course));
+    }
+  }, [course, dispatch]);
+
+  if (term == null || course == null || enrolments == null) {
     return <Loading />;
   }
 
@@ -36,19 +52,31 @@ export default (props: RouteComponentProps<MatchParams>) => {
       <Fab className={classes.fab} onClick={() => history.push(`/terms/${term.id}/courses/${courseId}/edit`)}>
         <EditIcon />
       </Fab>
+
+      <Table>
+        <TableHead>
+          <TableCell>Nr.</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Fehlstunden (Unentschuldigt)</TableCell>
+          <TableCell>Fehlende Hausaufgaben</TableCell>
+        </TableHead>
+
+        {enrolments.map((e, i) => (
+          <TableRow>
+            <TableCell>{i + 1}</TableCell>
+            <TableCell>{e.id}</TableCell>
+            <TableCell>0 / 0</TableCell>
+            <TableCell>0 / 0</TableCell>
+          </TableRow>
+        ))}
+      </Table>
     </div>
   );
 };
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      width: '95%',
-      marginTop: theme.spacing(3),
-      overflowX: 'auto',
-      margin: 'auto',
-      padding: 20,
-    },
+    root: {},
     table: {
       minWidth: 650,
     },
